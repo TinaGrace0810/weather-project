d574e1bc30e80ce58ef0c9e63cf93d31798d166b
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-06-18 14:34:59 +0100

    update

--- src/index.js
+++ src/index.js
@@ -48,14 +48,22 @@ function showWeather(response) {
   let percipitationElement = document.querySelector("#percipitation");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let iconElement = document.querySelector("#icon");
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
   descriptionElement.innerHTML = response.data.weather[0].description;
-  percipitationElement.innerHTML = response.data.weather[0].percipitation;
-  humidityElement.innerHTML = response.data.weather[0].humidity;
-  windElement.innerHTML = response.data.weather[0].wind;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed * 3.6);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function convertToFahrenheit(event) {
